d62256e87974c9f513d690911c200d7cd13bcd06
Fernando Kenji <kenjifernandoo@gmail.com>
2020-07-02 02:02:48 -0300

    sparkles: Challenge done

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -12,6 +12,14 @@ interface IProduct {
   quantity: number;
 }
 
+interface IHashedQuantity {
+  [key: string]: number;
+}
+
+interface IHashedPrice {
+  [key: string]: number;
+}
+
 interface IRequest {
   customer_id: string;
   products: IProduct[];
@@ -20,13 +28,63 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Invalid customer');
+    }
+
+    const existingProducts = await this.productsRepository.findAllById(
+      products,
+    );
+
+    if (existingProducts.length !== products.length) {
+      throw new AppError('Invalid product(s)');
+    }
+
+    const productsQuantity: IHashedQuantity = products.reduce(
+      (acc, product) => ({ ...acc, [product.id]: product.quantity }),
+      {},
+    );
+
+    const isQuantitySuficient = existingProducts.every(
+      product => product.quantity >= productsQuantity[product.id],
+    );
+
+    if (!isQuantitySuficient) {
+      throw new AppError('Insuficient quantities');
+    }
+
+    await this.productsRepository.updateQuantity(products);
+
+    const productsPrice: IHashedPrice = existingProducts.reduce(
+      (acc, product) => ({ ...acc, [product.id]: product.price }),
+      {},
+    );
+
+    const orderProducts = products.map(product => ({
+      product_id: product.id,
+      quantity: product.quantity,
+      price: productsPrice[product.id],
+    }));
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: orderProducts,
+    });
+
+    return order;
   }
 }
 
--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -9,6 +9,10 @@ interface IFindProducts {
   id: string;
 }
 
+interface IHashedQuantity {
+  [key: string]: number;
+}
+
 class ProductsRepository implements IProductsRepository {
   private ormRepository: Repository<Product>;
 
@@ -21,21 +25,63 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    const product = this.ormRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    const product = await this.ormRepository.findOne({
+      where: {
+        name,
+      },
+    });
+
+    return product;
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    const products_id = products.map(product => product.id);
+
+    const productsById = await this.ormRepository.find({
+      where: {
+        id: In(products_id),
+      },
+    });
+
+    return productsById;
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    const products_id = products.map(product => product.id);
+
+    const productsById = await this.ormRepository.find({
+      where: {
+        id: In(products_id),
+      },
+    });
+
+    const productsQuantity: IHashedQuantity = products.reduce(
+      (acc, product) => ({ ...acc, [product.id]: product.quantity }),
+      {},
+    );
+
+    const updatedProducts = productsById.map(product => ({
+      ...product,
+      quantity: product.quantity - productsQuantity[product.id],
+    }));
+
+    await this.ormRepository.save(updatedProducts);
+
+    return updatedProducts;
   }
 }
 
